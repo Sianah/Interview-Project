10cf672be94a703c16c8437c8dfc53a77f0b4763
Sianah <89411101+Sianah@users.noreply.github.com>
2023-06-24 17:44:30 -0500

    figuring out api integration process

---
+++ main.js
@@ -0,0 +1,17 @@
+/*const url = 'https://find-places-to-live.p.rapidapi.com/placesToLive?place=stamford-fairfield-ct&type=%3CREQUIRED%3E';
+fetch("https://find-places-to-live.p.rapidapi.com/placesToLive?place=stamford-fairfield-ct&type=%3CREQUIRED%3E");
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f653313567msh32357d701af50a2p1134e0jsnbce2acb2327e',
+		'X-RapidAPI-Host': 'find-places-to-live.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}*/
