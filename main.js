c66e60cd3a1983379e648d0c6b0617bffb517b13
Sianah <89411101+Sianah@users.noreply.github.com>
2023-06-24 21:27:09 -0500

    set up api. Should be working but getting error

--- main.js
+++ main.js
@@ -15,3 +15,35 @@ try {
 } catch (error) {
 	console.error(error);
 }*/
+window.addEventListener("load", (event) => {
+	const request_params = window.location.search;
+	getLocationData(request_params);
+	renderData();
+});
+
+async function getLocationData(params){
+	const location_field = document.getElementById('resultsArea');
+	const crime_field = document.getElementById('crimeRate');
+	const pop_field = document.getElementById('resultsAmount');
+	const education_field = document.getElementById('resultsEducation');
+
+	const url = 'https://find-places-to-live.p.rapidapi.com/placesToLive'+params;
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'f653313567msh32357d701af50a2p1134e0jsnbce2acb2327e',
+			'X-RapidAPI-Host': 'find-places-to-live.p.rapidapi.com'
+		}
+	};
+	try{
+		const response = await fetch(url, options);
+		const result = await response.json();
+		console.log(result);
+		location_field.value = result.message;
+		crime_field.value = result.message;
+		pop_field.value = result.message;
+		education_field.value = result.message;
+	} catch {
+		console.error(error);
+	}
+}
