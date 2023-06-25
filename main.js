2615fbb356e5d90d912ff1b29f39eabde42097d1
Sianah <89411101+Sianah@users.noreply.github.com>
2023-06-24 22:59:39 -0500

    bad change

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
-/*const url = 'https://find-places-to-live.p.rapidapi.com/placesToLive?place=stamford-fairfield-ct&type=%3CREQUIRED%3E';
-fetch("https://find-places-to-live.p.rapidapi.com/placesToLive?place=stamford-fairfield-ct&type=%3CREQUIRED%3E");
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'f653313567msh32357d701af50a2p1134e0jsnbce2acb2327e',
-		'X-RapidAPI-Host': 'find-places-to-live.p.rapidapi.com'
-	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}*/
+/*Getting the API setup*/
 window.addEventListener("load", (event) => {
 	const request_params = window.location.search;
 	getLocationData(request_params);
@@ -35,10 +19,13 @@ async function getLocationData(params){
 			'X-RapidAPI-Host': 'find-places-to-live.p.rapidapi.com'
 		}
 	};
+	
 	try{
 		const response = await fetch(url, options);
 		const result = await response.json();
 		console.log(result);
+		/*Subscription to 'Find Places to Live' Api no longer works despite multiple efforts
+	so commenting out what values should have returned.*/
 		location_field.value = result.message; /*result.Population.value;*/
 		crime_field.value = result.message;/*result.Crime & Safety.description;*/
 		pop_field.value = result.message;/*result.MedianRent.value;*/
